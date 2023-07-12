61b798c3c2fa96d011b2bcc2f0a96292460f5ed3
ianrs_19 <bot1deian@gmail.com>
2023-07-11 20:23:12 -0600

    aaaaaaaaa

--- app.js
+++ app.js
@@ -33,6 +33,27 @@ function updateCounter() {
 updateCounter();
 
 
+const switchnav = document.getElementById('show-nav');
+const mainm = document.getElementById('main');
+const navigation = document.getElementById('navigation');
+const bgimage = document.getElementById('bg-mn');
+let swonoff = false;
+
+switchnav.addEventListener('click', function () {
+    if(swonoff === false){
+        mainm.style.display = 'none';
+        navigation.style.display = 'flex';
+        bgimage.style.opacity = '0.6';
+        swonoff = true;
+    } else{
+        mainm.style.display = 'block';
+        navigation.style.display = 'none';
+        bgimage.style.opacity = '1';
+        swonoff = false;
+    }
+})
+
+
 
 
 
