372b01cddef530ce9784d7900c66535e1240f82c
ianrs_19 <bot1deian@gmail.com>
2023-07-11 20:40:33 -0600

    a

--- app.js
+++ app.js
@@ -37,21 +37,33 @@ const switchnav = document.getElementById('show-nav');
 const mainm = document.getElementById('main');
 const navigation = document.getElementById('navigation');
 const bgimage = document.getElementById('bg-mn');
+const dt = document.getElementById('dt');
+const rs = document.getElementById('rs');
+const lg = document.getElementById('lg');
+
 let swonoff = false;
 
+const turnoffnav = () =>{
+    mainm.style.visibility = 'visible';
+    navigation.style.visibility = 'hidden';
+    bgimage.style.opacity = '1';
+    swonoff = false;
+}
+
 switchnav.addEventListener('click', function () {
-    if(swonoff === false){
-        mainm.style.display = 'none';
-        navigation.style.display = 'flex';
+    if (swonoff === false) {
+        mainm.style.visibility = 'hidden';
+        navigation.style.visibility = 'visible';
         bgimage.style.opacity = '0.6';
         swonoff = true;
-    } else{
-        mainm.style.display = 'block';
-        navigation.style.display = 'none';
-        bgimage.style.opacity = '1';
-        swonoff = false;
+    } else {
+        turnoffnav();
     }
-})
+});
+
+dt.addEventListener('click', turnoffnav);
+rs.addEventListener('click', turnoffnav);
+lg.addEventListener('click', turnoffnav);
 
 
 
